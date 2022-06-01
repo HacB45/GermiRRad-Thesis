754d395098a2e9af05b5a2aa148c048869fe175d
HacB45 <hugoacbarros@ua.pt>
2022-06-01 18:03:08 +0100

    sensor properties parametrization v1.0

--- mylama_ws/src/germgrid/src/germgrid.cpp
+++ mylama_ws/src/germgrid/src/germgrid.cpp
@@ -6,50 +6,156 @@
 #include "lama/types.h"
 #include "lama/sdm/map.h"
 #include "lama/pose2d.h"
-
 #include <tf/tf.h>
+#include <math.h>
 
 lama::SimpleOccupancyMap *map1;
 
+// void computeIrradiatedRay(const Eigen::Vector3ui& from, const Eigen::Vector3ui& to, Eigen::VectorVector3ui& sink)
+// {
+//     if ( from == to ) return;
+
+//     Eigen::Vector3l error = Eigen::Vector3l::Zero();
+//     Eigen::Vector3l coord = from.cast<int64_t>();
+//     Eigen::Vector3l delta = to.cast<int64_t>() - coord;
+//     nav_msgs::OccupancyGrid occGridDataObs;
+//     const size_t num_occGridDataObs = occGridDataObs.size();
+//     unsigned int width = occGridDataObs.info.width;
+//     unsigned int height= occGridDataObs.info.height;
+    
+//     Eigen::Vector3l step = (delta.array() < 0).select(-1, Eigen::Vector3l::Ones());
+
+//     delta = delta.array().abs();
+//     int n = delta.maxCoeff() - 1;
+
+//     // maximum change of any coordinate
+//     for (int i = 0; i < n; ++i){
+//       // update errors
+//       error += delta;
+
+//       for (int j = 0; j < 3; ++j)
+//       {
+//           if ( (error(j) << 1) >= n ){
+//               coord(j) += step(j);
+//               error(j) -= n;
+//           }
+//       }
+      
+//       // save the coordinate until reach an obstacle
+//       for (unsigned int h = 0; h < height; ++h)
+//       {
+//         for (unsigned int w = 0; w < width;  ++w)
+//         {
+//           char value = occGridDataObs.data[w + h*width];
+//           if (value == 100) {
+//               return;
+//           }
+//           else
+//           {
+//             sink.push_back(coord.cast<uint32_t>());
+//           }
+//         }
+//       }  
+//     }
+// }
+
+
 /**
- * @brief Indicates the position of the robot (x,y)
+ * @brief Updates the area on the map irradianted by the UV light
  * 
+ * Openning Angle - [pi/6 , pi/2] 
+ * Direction Angle - [0 , 2*pi[ 
+ * Distance of Irradiation - 
+ * UV sensor position - Considered that it will be at the center of the robot
+ *          
  * @param msg 
  */
 void poseCallback(const geometry_msgs::PoseWithCovarianceStamped::ConstPtr& msg)
 {
-  ROS_INFO("Position of the Robot : [%f , %f]", msg->pose.pose.position.x,msg->pose.pose.position.y);
-  
-  /**
-   * 
-   * Initial Conditions:
-   * Openning Angle -> 90º
-   * Central Angle -> 0º (the sensor is turned to the front part of the robot)
-   * Distance of irradiation -> 2m
-   * The UV sensor is centered on the top of the robot
-   * 
-   */
-
   Eigen::VectorVector3ui fan_pos, occ_poses;
+  Eigen::Vector2d point0, point1; 
+  Eigen::Vector2d sensorPos;
+  double openAngle = M_PI/2;
+  double dirAngle = M_PI/2;
+  double rangeUV = 2; 
+  double uvAngleRightLimit, uvAngleLeftLimit;
   
-  lama::Pose2D pose(msg->pose.pose.position.x, msg->pose.pose.position.y, tf::getYaw(msg->pose.pose.orientation));
+  // Considered that it will be at the center of the robot
+  lama::Pose2D poseRobot(msg->pose.pose.position.x, msg->pose.pose.position.y, tf::getYaw(msg->pose.pose.orientation));
 
-  Eigen::Vector2d point0 = pose * Eigen::Vector2d(2, 2);
-  Eigen::Vector2d point1 = pose * Eigen::Vector2d(2,-2);
+  uvAngleLeftLimit = dirAngle + (openAngle/2);
+  uvAngleRightLimit = dirAngle - (openAngle/2);
 
-  map1->computeRay(map1->w2m(Eigen::Vector3d(point0(0), point0(1), 0.0)), 
-                   map1->w2m(Eigen::Vector3d(point1(0), point1(1), 0.0)), fan_pos);
 
-  // pos fan
+  // Point of the left range of the UV light
+  if (uvAngleLeftLimit >= (2*M_PI))
+  {
+    uvAngleLeftLimit = uvAngleLeftLimit - (2*M_PI); 
+    //QUADRANT IV
+    point0 = poseRobot * Eigen::Vector2d(cos(uvAngleLeftLimit*rangeUV), -sin(uvAngleLeftLimit*rangeUV));
+  }
+  else if ((dirAngle >= ((2*M_PI)/3)) && (dirAngle < (2*M_PI)))
+  {
+    //QUADRANT IV
+    point0 = poseRobot * Eigen::Vector2d(cos(uvAngleLeftLimit*rangeUV), -sin(uvAngleLeftLimit*rangeUV));
+  }
+  else if ((dirAngle >= M_PI) && (dirAngle < (2*M_PI)/3))
+  {
+    //QUADRANT III
+    point0 = poseRobot * Eigen::Vector2d(-cos(uvAngleLeftLimit*rangeUV), -sin(uvAngleLeftLimit*rangeUV));
+  }
+  else if ((uvAngleLeftLimit >= M_PI/2) && (uvAngleLeftLimit < M_PI))
+  {
+    //QUADRANT II
+    point0 = poseRobot * Eigen::Vector2d(-cos(uvAngleLeftLimit*rangeUV), -sin(uvAngleLeftLimit*rangeUV));
+  }
+  else 
+  {
+    //QUADRANT I
+    point0 = poseRobot * Eigen::Vector2d(cos(uvAngleLeftLimit*rangeUV), sin(uvAngleLeftLimit*rangeUV));
+  }  
 
+  
+  // Point of the right range of the UV light
+  if (uvAngleRightLimit < 0)
+  {
+    uvAngleRightLimit = (2*M_PI) + uvAngleRightLimit;
+    //QUADRANT I
+    point1 = poseRobot * Eigen::Vector2d(cos(uvAngleRightLimit*rangeUV), sin(uvAngleRightLimit*rangeUV));  
+
+  }
+  else if ((dirAngle >= 0) && (dirAngle < M_PI/2))
+  {
+    //QUADRANT I
+    point1 = poseRobot * Eigen::Vector2d(cos(uvAngleRightLimit*rangeUV), sin(uvAngleRightLimit*rangeUV));  
+  }
+  else if ((dirAngle >= M_PI/2) && (dirAngle < M_PI))
+  {
+    //QUADRANT II
+    point1 = poseRobot * Eigen::Vector2d(-cos(uvAngleRightLimit*rangeUV), sin(uvAngleRightLimit*rangeUV));  
+  }
+  else if ((dirAngle >= M_PI) && (dirAngle < ((2*M_PI)/3)))
+  {
+    //QUADRANT III
+    point1 = poseRobot * Eigen::Vector2d(-cos(uvAngleRightLimit*rangeUV), -sin(uvAngleRightLimit*rangeUV));  
+  }
+  else
+  {
+    //QUADRANT IV
+    point1 = poseRobot * Eigen::Vector2d(cos(uvAngleRightLimit*rangeUV), -sin(uvAngleRightLimit*rangeUV));  
+  }  
+
+  // Compute the points that represent the limit of the irradiantion light
+  map1->computeRay(map1->w2m(Eigen::Vector3d(point0(0), point0(1), 0.0)), map1->w2m(Eigen::Vector3d(point1(0), point1(1), 0.0)), fan_pos);
+
+
+  // Compute all the points that represent the area irradianted
   const size_t num_fan_pos = fan_pos.size();
   for (size_t i = 0; i < num_fan_pos; ++i){
-    
     map1->computeRay(map1->w2m(Eigen::Vector3d(msg->pose.pose.position.x, msg->pose.pose.position.y, 0.0)),fan_pos[i], occ_poses);
-  
   }
     
-
+  // Update the map with the calculated points
   const size_t num_occ_poses = occ_poses.size();
   for (size_t i = 0; i < num_occ_poses; ++i){
       map1->setFree(occ_poses[i]);
